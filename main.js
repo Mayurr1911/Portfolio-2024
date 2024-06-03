770c8cb1c8d0ca00f67f5e2f7d6c0ab3cc39df08
Mayur Rathod <157454491+Mayurr1911@users.noreply.github.com>
2024-06-03 16:22:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text",
+ {
+    strings: ["Frontend Developer" , "Graphic Designer" , "Web Designer" , "FreeLancer" , "Fitness Trainer" ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
